cf465075429f13b240d22a4365e3d95b8d70d3c4
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-11-08 23:05:28 +0700

    chore: button except

--- src/components/forum/HeadForum.jsx
+++ src/components/forum/HeadForum.jsx
@@ -26,21 +26,26 @@ const HeadForum = () => {
             </div>
           </div>
           {searchText && (
-            <button className="ml-2 px-4 py-2 text-md font-semibold text-white bg-[#6C7D41] rounded-lg hover:bg-[#5b6936] transition-all duration-300"
-              onClick={() => {
-                window.location.href = `/forum/cari/${searchText}`;
-              }
-            }>
+            <Link
+              to={`/forum/cari/${searchText}`}
+              className="ml-2 px-4 py-2 text-md font-semibold text-white bg-[#6C7D41] rounded-lg hover:bg-[#5b6936] transition-all duration-300"
+            >
               Cari
-            </button>
+            </Link>
           )}
         </div>
       </div>
       <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 w-full md:w-auto">
-        <Link to={'/forum/buat-diskusi'} className="flex items-center justify-center px-4 py-2 text-md font-semibold text-[#6C7D41] hover:text-white bg-transparent border border-[#6C7D41] rounded-lg hover:bg-[#6C7D41] transition-all duration-300 w-full md:w-auto">
+        <Link
+          to={"/forum/buat-diskusi"}
+          className="flex items-center justify-center px-4 py-2 text-md font-semibold text-[#6C7D41] hover:text-white bg-transparent border border-[#6C7D41] rounded-lg hover:bg-[#6C7D41] transition-all duration-300 w-full md:w-auto"
+        >
           <FaPlus className="mr-2" /> Buat Diskusi
         </Link>
-        <Link to={'/forum/diskusi-terakhir'} className="flex items-center justify-center px-4 py-2 text-md font-semibold text-[#6C7D41] hover:text-white bg-transparent border border-[#6C7D41] rounded-lg hover:bg-[#6C7D41] transition-all duration-300 w-full md:w-auto">
+        <Link
+          to={"/forum/diskusi-terakhir"}
+          className="flex items-center justify-center px-4 py-2 text-md font-semibold text-[#6C7D41] hover:text-white bg-transparent border border-[#6C7D41] rounded-lg hover:bg-[#6C7D41] transition-all duration-300 w-full md:w-auto"
+        >
           <FaList className="mr-2" /> Diskusi Terakhir
         </Link>
       </div>
