e8c1be0d0abb3308119b9e90abef7d6222850a42
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-11-09 15:48:35 +0700

    change: rekomendasi AI layout

--- src/components/dashboard/RekomendasiAI.jsx
+++ src/components/dashboard/RekomendasiAI.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TreeDeciduous as Plant } from "lucide-react";
+import { Cloud, Droplets, TreeDeciduous as Plant, ThermometerSun } from "lucide-react";
 
 const RekomendasiAI = () => {
   return (
@@ -23,6 +23,44 @@ const RekomendasiAI = () => {
             <p className="text-xs text-gray-600 mt-1">
               Rata-rata kelembaban di lokasi Anda saat ini ideal untuk menanam
               padi
+            </p>
+          </div>
+        </div>
+        <h4 className="text-sm font-medium mt-6">Rata-rata 3 Bulan Kedepan</h4>
+        <div className="flex items-center mt-4 p-4 bg-gray-50 rounded-xl">
+          <ThermometerSun className="w-14 h-14 text-[#6C7D41]" />
+          <div className="ml-4">
+            <div className="flex items-center gap-2">
+              <h4 className="font-medium">Suhu</h4>
+            </div>
+            <p className="text-sm font-medium text-[#6C7D41]">Sangat Ideal</p>
+            <p className="text-xs text-gray-600 mt-1">
+              Suhu rata-rata 3 bulan kedepan adalah <span className="font-bold">28°C</span>
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center mt-4 p-4 bg-gray-50 rounded-xl grid-cols-2">
+          <Droplets className="w-14 h-14 text-yellow-600" />
+          <div className="ml-4">
+            <div className="flex items-center gap-2">
+              <h4 className="font-medium">Kelembaban</h4>
+            </div>
+            <p className="text-sm font-medium text-yellow-600">Mendekati Ideal</p>
+            <p className="text-xs text-gray-600 mt-1">
+              Kelembaban rata-rata 3 bulan kedepan adalah <span className="font-bold">75%</span>
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center mt-4 p-4 bg-gray-50 rounded-xl">
+          <Cloud className="w-14 h-14 text-red-500" />
+          <div className="ml-4">
+            <div className="flex items-center gap-2">
+              <h4 className="font-medium">Curah Hujan</h4>
+              <span className="text-sm">60%</span>
+            </div>
+            <p className="text-sm font-medium text-red-500">Tidak Ideal</p>
+            <p className="text-xs text-gray-600 mt-1">
+              Curah hujan rata-rata 3 bulan kedepan adalah <span className="font-bold">60%</span>
             </p>
           </div>
         </div>
