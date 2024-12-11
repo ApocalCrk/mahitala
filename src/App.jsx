9decf066256bcce8ff97e3bac0209280c248a8bd
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-12-11 11:12:27 +0700

    chore: add react-helmet-async for improved document head management

--- src/App.jsx
+++ src/App.jsx
@@ -20,63 +20,73 @@ import CariDiskusiLayout from "./pages/forum/finddiscussion/CariDiskusiLayout";
 import { UserProvider } from "./utils/userContext";
 import ProtectedRoute from "./utils/middleware";
 
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 function App() {
   return (
-    <UserProvider>
-      <div className="flex flex-col min-h-screen">
-        <Routes>
-          <Route path="/" element={<ForecastDashboard />} />
-          <Route
-            path="*"
-            element={
-              <>
-                <Header />
-                <main className="flex-grow">
-                  <Routes>
-                    <Route path="/tentang-kami" element={<About />} />
-                    <Route path="/forum" element={<ForumDiskusi />} />
-                    <Route
-                      path="/forum/buat-diskusi"
-                      element={
-                        <ProtectedRoute>
-                          <BuatDiskusi />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path="/forum/diskusi-terakhir"
-                      element={
-                        <ProtectedRoute>
-                          <DiskusiTerakhir />
-                        </ProtectedRoute>
-                      }
-                    />
-                    <Route
-                      path="/forum/diskusi/:id"
-                      element={<DetailDiskusi />}
-                    />
-                    <Route path="/forum/kategori" element={<KategoriDiskusi />} />
-                    <Route
-                      path="/forum/kategori/:id"
-                      element={<DetailKategori />}
-                    />
-                    <Route
-                      path="/forum/diskusi-terbaru"
-                      element={<DiskusiTerbaruLayout />}
-                    />
-                    <Route
-                      path="/forum/cari/:keyword"
-                      element={<CariDiskusiLayout />}
-                    />
-                  </Routes>
-                </main>
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </div>
-    </UserProvider>
+    <HelmetProvider>
+      <UserProvider>
+        <Helmet>
+          <title>Mahitala App</title>
+          <meta name="title" content="Mahitala App" />
+          <meta name="description" content="Mahitala App adalah aplikasi penyedia solusi agrikultur berbasis teknologi yang membantu petani dalam perencanaan dan pengelolaan pertanian. Aplikasi ini menawarkan fitur seperti prediksi cuaca akurat, rekomendasi tanaman berbasis AI, serta analisis data pertanian untuk meningkatkan produktivitas dan efisiensi." />
+          <meta name="keywords" content="Mahitala App, aplikasi agrikultur, teknologi pertanian, prediksi cuaca, rekomendasi tanaman, AI untuk pertanian, analisis data pertanian, efisiensi pertanian, solusi agrikultur, produktivitas pertanian, inovasi teknologi pertanian, Infinite Learning, IL, Mahitala IL, Mahitala" />
+        </Helmet>
+        <div className="flex flex-col min-h-screen">
+          <Routes>
+            <Route path="/" element={<ForecastDashboard />} />
+            <Route
+              path="*"
+              element={
+                <>
+                  <Header />
+                  <main className="flex-grow">
+                    <Routes>
+                      <Route path="/tentang-kami" element={<About />} />
+                      <Route path="/forum" element={<ForumDiskusi />} />
+                      <Route
+                        path="/forum/buat-diskusi"
+                        element={
+                          <ProtectedRoute>
+                            <BuatDiskusi />
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path="/forum/diskusi-terakhir"
+                        element={
+                          <ProtectedRoute>
+                            <DiskusiTerakhir />
+                          </ProtectedRoute>
+                        }
+                      />
+                      <Route
+                        path="/forum/diskusi/:id"
+                        element={<DetailDiskusi />}
+                      />
+                      <Route path="/forum/kategori" element={<KategoriDiskusi />} />
+                      <Route
+                        path="/forum/kategori/:id"
+                        element={<DetailKategori />}
+                      />
+                      <Route
+                        path="/forum/diskusi-terbaru"
+                        element={<DiskusiTerbaruLayout />}
+                      />
+                      <Route
+                        path="/forum/cari/:keyword"
+                        element={<CariDiskusiLayout />}
+                      />
+                    </Routes>
+                  </main>
+                  <Footer />
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      </UserProvider>
+    </HelmetProvider>
   );
 }
 
