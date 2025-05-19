8b044875d694998de04ca3184c8b172610d3036d
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-05-19 21:06:11 +0700

    chore: forecast trouble

--- src/hooks/forecast/getDataForecast.jsx
+++ src/hooks/forecast/getDataForecast.jsx
@@ -1,11 +1,18 @@
 import axios from "axios";
+import axiosInstance from "../../utils/axiosInstance";
 import { API_URL } from "../../utils/Constants";
 
 export const getDataForecast = async ({ location }) => {
     const { latitude, longitude } = location;
+    const token = localStorage.getItem("token");
     try {
-        const res = await axios.post(API_URL+`/api/cuaca/forecast`, {latitude, longitude});
-        return res.data;
+        if (token) {
+            const res = await axiosInstance.post(`/api/cuaca/forecast`, {latitude, longitude});
+            return res.data;
+        } else {
+            const res = await axios.post(API_URL + `/api/cuaca/forecastNT`, {latitude, longitude});
+            return res.data;
+        }
     } catch (error) {
         return error;
     }
