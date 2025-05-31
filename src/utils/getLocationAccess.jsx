f656f54ae94188ef822a0e8a7f7cb0e8ff78bd34
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-01 01:25:38 +0700

    change reverse geolocation logic

--- src/utils/getLocationAccess.jsx
+++ src/utils/getLocationAccess.jsx
@@ -1,10 +1,20 @@
+import { API_URL } from "./Constants";
+
 const getLocation = async (latitude, longitude) => {
-    const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json&accept-language=id`;
+    // const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json&accept-language=id`; // nominatim 
+
+    // const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=id`; // bigdatacloud
+
+    // const url = `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&format=json&accept-language=id`; // geocode.maps.co
+
+    // const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json&accept-language=id&addressdetails=1`; // nominatim dengan address details
+
+    const url = API_URL + `/api/reverse-geocode?lat=${latitude}&lon=${longitude}`;
   
     try {
       const response = await fetch(url);
       const data = await response.json();
-  
+      
       if (data && data.address) {
         const city = data.address.city || data.address.town || data.address.village || "Kota tidak ditemukan";
         const province = data.address.state || "Provinsi tidak ditemukan";
