46bd254b87d4c240cf85b7d412d12fc8851292ed
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-01 18:52:10 +0700

    fix: cors origin conflict

--- src/hooks/forecast/getDataForecast.jsx
+++ src/hooks/forecast/getDataForecast.jsx
@@ -3,18 +3,25 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_URL } from "../../utils/Constants";
 
 export const getDataForecast = async ({ location }) => {
-    const { latitude, longitude } = location;
-    const token = localStorage.getItem("token");
-    
-    try {
-        if (token) {
-            const res = await axiosInstance.post(`/api/cuaca/forecast`, {latitude, longitude});
-            return res.data;
-        } else {
-            const res = await axios.post(API_URL + `/api/cuaca/forecastNT`, {latitude, longitude});
-            return res.data;
-        }
-    } catch (error) {
-        return error;
+  const { latitude, longitude } = location;
+  const token = localStorage.getItem("token");
+
+  try {
+    if (token) {
+      const res = await axiosInstance.post(`/api/cuaca/forecast`, {
+        latitude,
+        longitude,
+      });
+      return res.data;
+    } else {
+      const res = await axios.post(
+        API_URL + `/api/cuaca/forecastNT`,
+        { latitude, longitude },
+        { withCredentials: true }
+      );
+      return res.data;
     }
-}
+  } catch (error) {
+    return error;
+  }
+};
