76989bb27a32a8f2e86e81dffbbe3440a4808902
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-03 15:15:18 +0700

    change caching reverse geocode

--- src/hooks/forecast/getDataForecast.jsx
+++ src/hooks/forecast/getDataForecast.jsx
@@ -3,26 +3,30 @@ import { API_URL } from "../../utils/Constants";
 import axiosInstance from "../../utils/axiosInstance";
 
 export const getDataForecast = async ({ location }) => {
- console.log('Fetching weather data - attempt:', Date.now());
   const { latitude, longitude } = location;
-  const token = localStorage.getItem("token");
 
   try {
-    if (token) {
-      const res = await axiosInstance.post(`/api/cuaca/forecast`, {
+    const res = await axiosInstance.post(`/api/cuaca/forecast`, {
+      latitude,
+      longitude,
+    });
+    return res.data;
+  } catch (error) {
+    const res = await axios.get(`${API_URL}/api/cuaca/forecastNT`, {
+      params: {
         latitude,
         longitude,
-      });
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": API_URL,
+      },
+    });
+    if (res.status === 200) {
       return res.data;
     } else {
-      const res = await axios.get(API_URL + `/api/cuaca/forecastNT`, {
-        params: { latitude, longitude },
-      });
-      console.log('Response status:', res.status);
-    console.log('Response headers:', [...res.headers.entries()]);
-      return res.data;
+      console.error("Error fetching forecast data:", res.statusText);
+      return null;
     }
-  } catch (error) {
-    return error;
   }
 };
