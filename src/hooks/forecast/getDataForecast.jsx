81fc607278701a5dc0c6fdff38a2305730d5962e
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-03 13:06:29 +0700

    fix: refactor getHargaKomoditas to improve axios request formatting

--- src/hooks/forecast/getDataForecast.jsx
+++ src/hooks/forecast/getDataForecast.jsx
@@ -3,18 +3,28 @@ import { API_URL } from "../../utils/Constants";
 import axiosInstance from "../../utils/axiosInstance";
 
 export const getDataForecast = async ({ location }) => {
-    const { latitude, longitude } = location;
-    const token = localStorage.getItem("token");
-    
-    try {
-        if (token) {
-            const res = await axiosInstance.post(`/api/cuaca/forecast`, {latitude, longitude});
-            return res.data;
-        } else {
-            const res = await axios.post(API_URL + `/api/cuaca/forecastNT`, {latitude, longitude});
-            return res.data;
-        }
-    } catch (error) {
-        return error;
+  const { latitude, longitude } = location;
+  const token = localStorage.getItem("token");
+
+  try {
+    if (token) {
+      const res = await axiosInstance.post(`/api/cuaca/forecast`, {
+        latitude,
+        longitude,
+      });
+      return res.data;
+    } else {
+      const res = await axios.post(API_URL + `/api/cuaca/forecastNT`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": API_URL,
+        },
+        latitude,
+        longitude,
+      });
+      return res.data;
     }
-}
+  } catch (error) {
+    return error;
+  }
+};
--- src/hooks/forum/getHargaKomoditas.jsx
+++ src/hooks/forum/getHargaKomoditas.jsx
@@ -3,12 +3,12 @@ import { API_URL } from "../../utils/Constants";
 
 export const getHargaKomoditas = async () => {
   try {
-    const res = await axios.get(`${API_URL}/api/forum/harga-komoditas`,
-      {
+    const res = await axios.get(`${API_URL}/api/forum/harga-komoditas`, {
+      headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": API_URL,
-      }
-    );
+      },
+    });
 
     return res.data;
   } catch (error) {
