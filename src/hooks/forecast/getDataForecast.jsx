1911726252a96802210c072674c05d14d0d23d1d
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-03 16:07:43 +0700

    fix: add CORS headers to enhance API security

--- src/hooks/forecast/getDataForecast.jsx
+++ src/hooks/forecast/getDataForecast.jsx
@@ -7,18 +7,11 @@ export const getDataForecast = async ({ location }) => {
   const token = localStorage.getItem("token");
 
   try {
-    if (token) {
-      const res = await axiosInstance.post(`/api/cuaca/forecast`, {
-        latitude,
-        longitude,
-      });
-      return res.data;
-    } else {
-      const res = await axios.get(API_URL + `/api/cuaca/forecastNT`, {
-        params: { latitude, longitude },
-      });
-      return res.data;
-    }
+    const res = await axiosInstance.post(`/api/cuaca/forecast`, {
+    latitude,
+    longitude,
+    });
+    return res.data;
   } catch (error) {
     return error;
   }
