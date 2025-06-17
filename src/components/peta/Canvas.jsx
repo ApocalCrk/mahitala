9613613dcc9938ddc04c5dae04b275e01b7661cf
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-06-17 18:27:46 +0700

    remove edit icon on canvas

--- src/components/peta/Canvas.jsx
+++ src/components/peta/Canvas.jsx
@@ -1961,20 +1961,6 @@ const Canvas = ({ location, nowData }) => {
                         <h3 className="text-base font-semibold text-blue-800">
                           {poly.fieldName}
                         </h3>
-                        <button
-                          className="text-gray-500 hover:text-gray-700"
-                          onClick={() => {
-                            setType("edit");
-                            setFieldName(poly.fieldName);
-                            setSoilType(poly.soilType);
-                            setCropId(poly.cropId);
-                            setCropDate(poly.cropDate);
-                            setEstimatedTime(poly.estimatedTime);
-                            setPolygonPoints(poly.coords);
-                          }}
-                        >
-                          <Edit className="h-4 w-4" />
-                        </button>
                       </div>
 
                       {/* Weather */}
