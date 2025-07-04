b21e925fa5deb5a50eb4c4b901b3356a4dae781c
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2025-07-04 16:04:44 +0700

    chore: forum pagination bug

--- src/components/forum/newsdiscussion/SubComponents/ItemDiskusiTerbaru.jsx
+++ src/components/forum/newsdiscussion/SubComponents/ItemDiskusiTerbaru.jsx
@@ -10,7 +10,7 @@ const ItemDiskusiTerbaru = ({ discussion }) => {
         <div className="p-6">
           <div className="flex items-center justify-between mb-3">
             <span className="text-sm text-gray-500">
-              Oleh {discussion.user}
+              Oleh {discussion.username}
             </span>
             <span className="text-sm text-gray-500">
               {checkWaktu(discussion.tgl_dibuat)}
